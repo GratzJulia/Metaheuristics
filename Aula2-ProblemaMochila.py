5bddc5ed4d96d70eb59de3cc91d99453af9aec4b
Julia Gratz <10821353721@cefet-rj.br>
2024-09-29 23:10:03 -0300

    Aula2 - heuristica de exemplo

--- Aula2-ProblemaMochila.py
+++ Aula2-ProblemaMochila.py
@@ -9,24 +9,26 @@ def canAllItemsBeTaken(Ws: list, capacity: int):
     return True
 
 def solveMochila(len: int, P, W, C: int, s = []):
-    sortingCriteria = [{'criteria': float(P[i]/W[i]), 'p': P[i], 'w': W[i]} for i in range(len)]
+    sortingCriteria = [{'criteria': float(P[i]/W[i]), 'p': int(P[i]), 'w': int(W[i])} for i in range(len)]
     sortingCriteria.sort(key=lambda x: x['criteria'], reverse=True)
 
     for i in range(len):
-        if C - sortingCriteria[i]['w'] > 0:
-            s.append(i+1)
+        if C - sortingCriteria[i]['w'] >= 0:
+            s.append(sortingCriteria[i])
             C = C - sortingCriteria[i]['w']
     
     return s
 
 def main():
-    csv = read_csv('./input-data/TestExample-ProblemaDaMochilaClassico.csv', sep=';')
+    # csv = read_csv('./input-data/TestExample-ProblemaDaMochilaClassico.csv', sep=';')
+    csv = read_csv('./input-data/Example-Aula2-ProblemaDaMochilaClassico.csv', sep=';')
 
     lenItems = csv.get('Items').size
     allW = csv.get('w') # grandeza relacionada ao espaco fisico
     allP = csv.get('p') # valor do item (recompensa)
     s = []
-    c = (7)
+    # c = (7)
+    c = (9)
 
     if canAllItemsBeTaken(allW, c): 
         s = [i+1 for i in range(lenItems)]
