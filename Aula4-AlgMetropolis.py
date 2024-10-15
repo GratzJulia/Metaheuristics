16f4ea902c599c074ed62cb183a7a6682c5800f0
Julia Gratz <10821353721@cefet-rj.br>
2024-10-15 07:52:58 -0300

    kruskal refactor

--- Aula4-AlgMetropolis.py
+++ Aula4-AlgMetropolis.py
@@ -4,51 +4,50 @@ class Grafo:
         self.V = vertices
         self.arestas = []
 
-    def add_aresta(self, i, j, peso):
-        self.arestas.append((peso, i, j))
+    def add_aresta(self, i, j, custo):
+        self.arestas.append((custo, i, j))
 
-    def kruskal(self):
-        self.arestas.sort()
-        pai = {}
-        rank = {}
+def kruskal(G: Grafo):
+    G.arestas.sort()
+    pai = {}
+    rank = {}
 
-        for j in range(1, self.V+1):
-            pai[j] = j
-            rank[j] = 0
+    for j in range(1, G.V+1):
+        pai[j] = j
+        rank[j] = 0
 
-        def find(v):
-            if pai[v] != v:
-                pai[v] = find(pai[v])
-            return pai[v]
+    def find(v):
+        if pai[v] != v:
+            pai[v] = find(pai[v])
+        return pai[v]
 
-        def union(i, j):
-            raiz_i = find(i)
-            raiz_j = find(j)
+    def union(i, j):
+        raiz_i = find(i)
+        raiz_j = find(j)
 
-            if raiz_i != raiz_j:
-                # União por rank
-                if rank[raiz_i] > rank[raiz_j]:
-                    pai[raiz_j] = raiz_i
-                elif rank[raiz_i] < rank[raiz_j]:
-                    pai[raiz_i] = raiz_j
-                else:
-                    pai[raiz_j] = raiz_i
-                    rank[raiz_i] += 1
+        if raiz_i != raiz_j:
+            # União por rank
+            if rank[raiz_i] > rank[raiz_j]:
+                pai[raiz_j] = raiz_i
+            elif rank[raiz_i] < rank[raiz_j]:
+                pai[raiz_i] = raiz_j
+            else:
+                pai[raiz_j] = raiz_i
+                rank[raiz_i] += 1
 
-        AGM = []  # Arvore Geradora Minima
-        count_aresta = 0
+    AGM = []  # Arvore Geradora Minima
+    count_aresta = 0
 
-        for peso, i, j in self.arestas:
-            forma_ciclo = find(i) == find(j)
-            if not forma_ciclo:
-                union(i, j)
-                AGM.append((i, j, peso))
-                count_aresta += 1
+    for peso, i, j in G.arestas:
+        forma_ciclo = find(i) == find(j)
+        if not forma_ciclo:
+            union(i, j)
+            AGM.append((i, j, peso))
+            count_aresta += 1
 
-            if count_aresta == self.V - 1: break
+        if count_aresta == G.V - 1: break
 
-        return AGM
-
+    return AGM
 
 if __name__ == "__main__":
     g = Grafo(7)    # grafo de exemplo do slide
@@ -62,6 +61,6 @@ if __name__ == "__main__":
     g.add_aresta(5, 6, 2)
     g.add_aresta(6, 7, 5)
 
-    agm = g.kruskal()
+    agm = kruskal(g)
     print("Arestas da Árvore Geradora Mínima:")
-    [print(f"{i}-{j}: {peso}") for i, j, peso in agm]
+    [print(f"{i}-{j}: {custo}") for i, j, custo in agm]
